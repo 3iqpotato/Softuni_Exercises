78880c52921b6bdb0ab5af8fdb1659060f0595af
3iqpotato <103383079+3iqpotato@users.noreply.github.com>
2023-10-12 20:12:48 +0300

    Delete dumb_things directory

--- dumb_things/are_you_dumb_game/Are_You_DumP.py
+++
@@ -1,6 +0,0 @@
-from dumb_things.are_you_dumb_game.start_screen import window
-from dumb_things.are_you_dumb_game.render_buttons import create_entry
-
-if __name__ == '__main__':
-    create_entry()
-    window.mainloop()
--- dumb_things/are_you_dumb_game/cleaning.py
+++
@@ -1,5 +0,0 @@
-from dumb_things.are_you_dumb_game.render_buttons import frame
-
-
-def clean():
-    frame.delete('all')
